fdbd20f05a6637e618e0a82cd09e55c01ddd7b09
unknown <krutko7779@gmail.com>
2025-05-18 15:14:49 +0300

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,58 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+// Переход на страницу благодарности после отправки формы
+document.addEventListener( 'formSent', function( event ) {
+	location = 'https://webkrutko.by/thank-you-page.html';
+}, false );
+
+// Защита формы от ботов через пустое поле
+// Форма в контактах
+if ( document.querySelector('.btn') ) {  // Проверяем наличие элемента на странице
+		let code = document.querySelector('#code'); // Получаем скрытый input
+		document.querySelector('.btn').onclick = function(){ // Клик по кнопке отправки
+			code.value = 'NOSPAM'; // Подставляем значение в value инпута	
+	};
+}
+  // Форма в popup
+  if ( document.querySelector('.btn-popup') ) {  // Проверяем наличие элемента на странице
+		let codePopup = document.querySelector('#code-popup'); // Получаем скрытый input
+		document.querySelector('.btn-popup').onclick = function(){ // Клик по кнопке отправки
+			codePopup.value = 'NOSPAM'; // Подставляем значение в value инпута	
+		};
+  }
+
+    // Форма в брифе
+	 if ( document.querySelector('.btn-brief') ) {  // Проверяем наличие элемента на странице
+		let codePopup = document.querySelector('#code-brief'); // Получаем скрытый input
+		document.querySelector('.btn-brief').onclick = function(){ // Клик по кнопке отправки
+			codePopup.value = 'NOSPAM'; // Подставляем значение в value инпута	
+		};
+  }
+
+  // Кнопка НАЗАД
+if ( document.querySelector('.button-back') ) {  // Проверяем наличие элемента на странице
+	document.querySelector('.button-back').onclick = function(){ // Клик по кнопке НАЗАД
+		window.history.go(-2); // возвращаемся назад
+	};
+}
+
+// Добавление класса _navigator-active к активному пункту меню
+const currentPage = location.href;
+const allAs = document.querySelectorAll('.menu__link');
+const allAsLength = allAs.length
+
+for (let i = 0; i < allAsLength; i++) {
+	if (allAs[i].href === currentPage) {
+		allAs[i].className = "menu__link _navigator-active";
+	}
+}
+
+
+
+
+
+
+
